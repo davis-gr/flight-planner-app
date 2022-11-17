8259c902a0641eb28f49183fca77aaae401b7670
davis-gr <davis.grundmanis@gmail.com>
2022-11-17 11:57:32 +0200

    initializing empty repository

---
+++ src/main/java/io/codelex/flightplannerapp/FlightPlannerAppApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.flightplannerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlannerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightPlannerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codelex/flightplannerapp/FlightPlannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelex.flightplannerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
